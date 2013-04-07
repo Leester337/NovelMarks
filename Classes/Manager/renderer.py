c09968ff4cb32f7e5165c927e10ff2716fb49e30
John Kaczor <johngkhs@gmail.com>
2013-04-07 16:33:25 -0500

    Added node drawable classes

--- Classes/Manager/renderer.py
+++ Classes/Manager/renderer.py
@@ -8,6 +8,21 @@ __author__ = 'john'
 class Renderer:
 
     def __init__(self):
-        self.win = GraphWin("My GUI Program", 600, 480)
+        self.width = 600
+        self.height = 480
+        self.win = GraphWin("My GUI Program", self.width, self.width)
         self.win.getMouse()
+
+    def convert_node_hierarchy(self, Node):
+        pass
+
+    def draw(self, Node):
+        pass
+        # draw the entire hierarchy
+
+    def get_object_clicked(self):
+        click_point = getMouse()
+        # return the node at the click position
+
+
 
---
+++ Classes/Model/nodedrawable.py
@@ -0,0 +1,45 @@
+__author__ = 'john'
+import sys
+sys.path.append('../../Lib/')
+
+from datetime import datetime
+from graphics import *
+
+class NodeDrawable():
+
+    def __init__(self, name, date, clickCount, color, radius, position):
+        self.name = name
+        self.date = date
+        self.clickCount = clickCount
+        self.color = color
+        self.radius = radius
+        self.position = position
+
+class FolderDrawable(NodeDrawable):
+
+    def __init__(self,
+                 name = '',
+                 date = datetime.now(),
+                 clickCount = 0,
+                 color = 0,
+                 radius = 0,
+                 position = Point(0,0),
+                 children = None):
+        Node.__init__(self, name, date, clickCount, color, radius, position)
+        if children is None:
+            self.children = []
+        else:
+            self.children = children
+
+class NodeDrawable(NodeDrawable):
+
+    def __init__(self,
+                 name = '',
+                 date = datetime.now(),
+                 clickCount = 0,
+                 color = 0,
+                 radius = 0,
+                 position = Point(0,0),
+                 url = ''):
+        Node.__init__(self, name, date, clickCount, color, radius, position)
+        self.url = url
